12842d2ded362b9dc6006868043d6efe06d9d07e
dmpalyvos <dm.palyvos@gmail.com>
2015-11-30 14:29:21 +0200

    Added custom mongoose setters for price/currency in product schema

---
+++ fx.js
@@ -0,0 +1,8 @@
+//Exchange Rates Function
+module.exports = function() {
+    return {
+        USD: 1,
+        EUR: 1.1,
+        GBP: 1.5
+    };
+};
--- product.js
+++ product.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Category = require('./category');
+var fx = require('./fx');
 
+//Schema for products
 var productSchema = {
     name: {
         type: String,
@@ -13,16 +15,47 @@ var productSchema = {
     price: {
         amount: {
             type: Number,
-            required: true
+            required: true,
+            set: function(newAmt) {
+                this.internal.approximatePriceUSD = newAmt / (fx()[this.price.currency] || 1);
+                return newAmt;
+            }
         },
         currency: {
             type: String,
             enum: ['USD', 'EUR', 'GBP'],
-            required: true
+            required: true,
+            set: function(newCurrency) {
+                this.internal.approximatePriceUSD = 
+                    this.internal.approximatePriceUSD / (fx()[newCurrency] || 1);
+                    return newCurrency;
+            }
         }
     },
-    category: Category.categorySchema
+    category: Category.categorySchema,
+    internal: {
+        approximatePriceUSD: Number
+    }
 };
 
-module.exports = new mongoose.Schema(productSchema);
+var schema = new mongoose.Schema(productSchema);
+
+/*
+ * Combine currency symbol and price amount into
+ * human readable format. For example 25 USD 
+ * returns "$25"
+ */
+
+var currencySymbols = {
+    'USD': '$',
+    'EUR': '€',
+    'GBP': '£'   
+};
+
+schema.virtual('displayPrice').get(function() {
+    return currencySymbols[this.price.currency] + '' + this.price.amount;
+});
+
+// Exports
+module.exports = schema;
 module.exports.productSchema = productSchema;
