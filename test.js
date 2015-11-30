71a5b79c81afe2802ee0874f0b181fe9f758e3fe
dmpalyvos <dm.palyvos@gmail.com>
2015-11-30 13:08:52 +0200

    Added mongoose schemas for product, category and user

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 var assert = require('assert');
 
 describe('Feature 1', function() {
-	
+
 	it('1 does not equal 2', function(done){
 		assert.notEqual(1, 2, 'One does not equal 2');
 		done();
 	});
-	
+
 	it('1+1 equals 2', function(done) {
 		assert.equal(1+1, 2);
 		done();
 	});
-	 
+
 });
