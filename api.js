b559dd4232e751b9ed9b946ae647305b1db0ef77
dmpalyvos <dm.palyvos@gmail.com>
2015-12-01 11:03:56 +0200

    Added more generic handlers in API and product model in models.js.

--- api.js
+++ api.js
@@ -5,32 +5,42 @@ module.exports = function(wagner) {
     
     var api = express.Router();
     
+    function handleOne(property, resp, error, result) {
+        if (error) {
+            return resp.
+                status(status.INTERNAL_SERVER_ERROR).
+                json( {error: error.toString() });
+        }
+        if (!result) {
+            return resp.
+                status(status.NOT_FOUND).
+                json( { error: 'Not Found' } );
+        }
+        var json = {};
+        json[property] = result;
+        resp.json(json);
+    }
+    
+    function handleMany(properties, resp, error, result) {
+         if (error) {
+            return resp.
+                status(status.INTERNAL_SERVER_ERROR).
+                json( { error: error.toString() } );
+         }
+         var json = {};
+         json[properties] = result;
+         resp.json(json);
+
+    }
+    
     /*
      * Retrieve category by id
      */
     api.get('/category/id/:id', wagner.invoke(function(Category) {
         return function(req, res) {
-            
-            Category.findOne( { _id: req.params.id }, function(error, category) {
-                
-                if (error) {
-                    return res.
-                        status(status.INTERNAL_SERVER_ERROR).
-                        json( { error: error.toString() } );
-                }
-                
-                if (!category) {
-                    return res.
-                        status(status.NOT_FOUND).
-                        json( { error: 'Not Found' });
-                }
-                
-                res.json( { category: category });
-                
-            });
-            
+            Category.findOne( { _id: req.params.id },  
+                handleOne.bind(null, 'category', res) );
         };
-        
     }));
     
     /*
@@ -38,25 +48,12 @@ module.exports = function(wagner) {
      */
     api.get('/category/parent/:id', wagner.invoke(function(Category) {
         return function(req, res) {
-            
             Category.find( { parent: req.params.id }).
                 sort( { _id: 1 } ).
-                exec(function(error, categories) {
-
-                    if (error) {
-                        return res.
-                            status(status.INTERNAL_SERVER_ERROR).
-                            json( { error: error.toString() } );
-                    }
-
-                    res.json( { categories: categories });
-
-            });
-            
+                exec(handleMany.bind(null, 'categories', res));            
         };
-        
     }));
-    
+      
     
     return api;
 };
--- models.js
+++ models.js
@@ -1,18 +1,26 @@
 var mongoose = require('mongoose');
-var categorySchema = require('./category');
+var _ = require('underscore');
 
 module.exports = function(wagner) {
+    var categorySchema = require('./category');
+    var productSchema = require('./product');
     
     mongoose.connect('mongodb://localhost:27017/test');
     
     var Category =  mongoose.model('Category', categorySchema, 'categories');
+    var Product = mongoose.model('Product', productSchema, 'products');
     
-    wagner.factory('Category', function() {
-        return Category;
+    var models = {
+        Category: Category,
+        Product: Product
+    };
+    
+    _.each(models, function(value, key) {
+        wagner.factory(key, function() {
+            return value;
+        });
     });
     
-    return {
-        Category: Category
-    };
+    return models;
     
 };
