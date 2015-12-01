18ca3b848b97f15204f10d9869e57a87d74c4b35
dmpalyvos <dm.palyvos@gmail.com>
2015-12-01 11:52:10 +0200

    Added product API and related tests.

--- api.js
+++ api.js
@@ -54,6 +54,36 @@ module.exports = function(wagner) {
         };
     }));
       
+    /*
+     * Retrieve product by id
+     */
+    api.get('/product/id/:id', wagner.invoke(function(Product) {
+        return function(req, res) {
+            Product.findOne( { _id: req.params.id },  
+                handleOne.bind(null, 'product', res) );
+        };
+    }));
+    
+    /*
+     * Retrieve product by category in which it belongs
+     */
+    api.get('/product/category/:id', wagner.invoke(function(Product) {
+        return function(req, res) {
+            //Default sort by name
+            var sort = { 'name' : 1 };
+            //Allow the user to specify sort by price
+            if (req.query.price === '-1') {
+                sort = { 'internal.approximatePriceUSD': 1 };
+            }
+            else if (req.query.price === '-1') {
+                sort = { 'internal.approximatePriceUSD': -1 };
+            }
+            
+            Product.find( { 'category.ancestors': req.params.id } ).
+                sort(sort).
+                exec(handleMany.bind(null, 'products', res));
+        };
+    }));
     
     return api;
 };
--- test.js
+++ test.js
@@ -5,10 +5,11 @@ var superagent = require('superagent');
 
 var URL_ROOT = 'http://localhost:3000';
 
-describe('Category API', function() {
+describe('API', function() {
 
     var server;
     var Category;
+    var Product;
     
     /* 
      * Initialize Server before tests
@@ -20,9 +21,9 @@ describe('Category API', function() {
        
         app.use(api);
         server = app.listen(3000);
-        
+
         Category = models.Category; 
-        
+        Product = models.Product;
     });
     
     /*
@@ -41,10 +42,14 @@ describe('Category API', function() {
             assert.ifError(error);
             done();
         });
+        Product.remove({}, function(error) {
+            assert.ifError(error);
+            done();
+        });
     });
     
     /*
-     * Tests 
+     * Category Tests
      */
     it('Can load a category by its id', function(done) {
         Category.create( {_id: 'Electronics' }, function(error, doc) {
@@ -59,9 +64,8 @@ describe('Category API', function() {
                 assert.ok(result.category);
                 assert.equal(result.category._id, 'Electronics');
             });
-            done();
        });
-       
+       done();
     }); 
     
     it('Can load all categories that have a certain parent', function(done) {
@@ -85,10 +89,97 @@ describe('Category API', function() {
                 assert.equal(result.categories.length, 2);
                 assert.equal(result.categories[0]._id, 'Laptops');
                 assert.equal(result.categories[1]._id, 'Phones');
-                done();
-            })
-       })
-       
+            });
+       });
+       done();
     });
 
+
+    /* 
+     * Product Tests
+     */
+    it('Can load a product by its id', function(done) {
+        //Create product
+        var PRODUCT_ID = '000000000000000000000001';
+        var prod = {
+            name: 'LG G2 Mini',
+            _id: PRODUCT_ID,
+            price: {
+                amount: 150,
+                currency: 'EUR'
+            }
+        };
+        Product.create(prod, function(error, doc) {
+            assert.ifError(error);
+            var url = URL_ROOT + '/product/id/' + PRODUCT_ID;
+            var result;
+            superagent.get(url, function(error, res) {
+                assert.ifError(error);
+                assert.doesNotThrow(function() {
+                    result = JSON.parse(res.text);
+                });
+                assert.ok(result.product);
+                assert.equal(result.product._id, PRODUCT_ID);
+            });
+       });
+       done();
+    });
+    
+    it('Can load a product by in a category with sub-categories', function(done) {
+        //Create products/categories
+        var categories = [
+            { _id: 'Electornics' },
+            { _id: 'Phones', parent: 'Electronics' },
+            { _id: 'Laptops', parent: 'Electronics' },
+            { _id: 'Bacon' }
+        ];
+        var products = [
+            {
+                name: 'LG G2 Mini',
+                category: { _id: 'Phones', ancestors: ['Electronics', 'Phones'] },
+                price: {
+                    amount: 300,
+                    currency: 'USD'
+                }
+            },
+            {
+                name: 'Asus Zenbook Prime',
+                category: { _id: 'Laptops', ancestors: ['Electronics', 'Laptops'] },
+                price: {
+                    amount: 1500,
+                    currency: 'EUR'
+                }
+            },
+            {
+                name: 'Pig 3000',
+                category: { _id: 'Bacon', ancestors: ['Bacon'] },
+                price: {
+                    amount: 150,
+                    currency: 'GBP'
+                }
+            }
+        ];
+
+        //Create the categories
+        Category.create(categories, function(error, categories) {
+            assert.ifError(error);
+            //Create the products
+            Product.create(products, function(error, products) {
+                assert.ifError(error);
+                var url = URL_ROOT + '/product/category/Electronics';
+                superagent.get(url, function(error, res) {
+                    assert.ifError(error);
+                    var result;
+                    assert.doesNotThrow(function() {
+                        result = JSON.parse(res.text);
+                    });
+                    assert.equal(result.products.length, 2);
+                    assert.equal(result.products[0].name, 'Asus Zenbook Prime');
+                    assert.equal(result.products[1].name, 'LG G2 Mini');
+                });
+            }); 
+        });
+       done();
+    });
+    
 });
